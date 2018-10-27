9481a18e5cce3b1096f13294a594f6c3fcf9a2cb
JotaGalera <javigalera94@gmail.com>
2018-10-27 19:25:03 +0200

    Clase más completa

--- src/mainProject.py
+++ src/mainProject.py
@@ -5,14 +5,41 @@ app = Flask(__name__)
 
 class Informatic:
 
-    def __init__(self):
-        self.name = " "
-        self.cv = " "
-        self.edad = " "
+    def __init__(self,name,cv,edad):
+        self.name = name
+        self.cv = cv
+        self.age = edad
 
     @app.route('/status')
     def statusFun(self):
         return True
+
+    #Modificaciones de datos propios de cada informatico
+    @app.route('/chname')
+    def changeName(self,new_name):
+        if(self.name != new_name and len(new_name)>2):
+            self.name = new_name
+        return self.name
+
+    @app.route('/chcv')
+    def changeCv(self,new_cv):
+        if(self.cv != new_cv):
+            self.cv = new_cv
+        return self.cv
+
+    @app.route('/chage')
+    def changeAge(self,new_age):
+        if(self.age != new_age):
+            self.age = new_age
+        return self.age
+
+    #Mostrando datos
+    @app.route('/showAll')
+    def showData(self):
+        if(len(self.name)>0 and len(self.cv)>0 and self.age>0):
+            print("Nombre:",self.name)
+            print("CV",self.cv)
+            print("Edad",self.age)
 
 if __name__ == "__main__":
   app.run(debug = True, host='0.0.0.0')
--- src/test_Status.py
+++ src/test_Status.py
@@ -1,4 +1,3 @@
-#import bin.main
 import unittest
 import flask
 from mainProject import Informatic
@@ -7,10 +6,20 @@ app = flask.Flask(__name__)
 
 class testProject(unittest.TestCase):
     def setUp(self):
-        self.inf = Informatic()
+        self.inf = Informatic("Carlos","CV old",18)
+        return self.inf
 
     def test_Status(self):
         self.assertEqual(self.inf.statusFun(),True,"Devuelve OKEY")
+
+    def test_changeName(self):
+        self.assertEqual(self.inf.changeName("Javier"),"Javier","Nombre cambiado")
+
+    def test_changeCv(self):
+        self.assertEqual(self.inf.changeCv("Cambio mi cv"),"Cambio mi cv","CV cambiado")
+
+    def test_changeAge(self):
+        self.assertEqual(self.inf.changeAge(25),25,"Edad cambiada")
 
 if __name__ == '__main__':
     unittest.main()
