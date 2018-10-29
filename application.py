8b20cddf14c4a7f43dfb666f3ee3045fdf9615f1
JotaGalera <javigalera94@gmail.com>
2018-10-29 20:05:32 +0100

    Cambio nombre

---
+++ application.py
@@ -0,0 +1,14 @@
+from flask import Flask,request
+import json
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def js():
+    dato={ "status": "OK" }
+    asd = json.dumps(dato)
+    return asd
+
+if __name__ == "__main__":
+    app.run()
+ 
